264cd916f6f092c317c117892fa0c902514201fc
Danny <dantheado@gmail.com>
2020-11-25 15:39:14 +0000

    Made binary search tree

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -53,16 +53,30 @@ class BinarySearchTree {
     if (!found) return undefined;
     return current;
   }
+
+  // Breadth First Search
+  BFS() {
+    let node = this.root,
+      data = [],
+      queue = [];
+
+    queue.push(node);
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
 }
 
 let tree = new BinarySearchTree();
 tree.insert(10);
-tree.insert(5);
-tree.insert(13);
-tree.insert(2);
-tree.insert(7);
-tree.insert(11);
-tree.insert(16);
-tree.insert(1);
+tree.insert(6);
+tree.insert(3);
+tree.insert(15);
+tree.insert(8);
+tree.insert(20);
 
-console.log(tree.find(13));
+console.log(tree.BFS());
