35340a5d101112ad5f0aab4b33201cf253dc7c60
Dan <dantheado@gmail.com>
2021-09-28 14:28:56 +0100

    Added keys and values methods

--- HashTable.js
+++ HashTable.js
@@ -41,14 +41,49 @@ class HashTable {
 
     return undefined;
   }
+
+  keys() {
+    let keysArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          // Prevent duplicate values in keysArr
+          if (!keysArr.includes(this.keyMap[i][j][0])) {
+            keysArr.push(this.keyMap[i][j][0]);
+          }
+        }
+      }
+    }
+    return keysArr;
+  }
+
+  values() {
+    let valuesArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          // Prevent duplicate values in valuesArr
+          if (!valuesArr.includes(this.keyMap[i][j][1])) {
+            valuesArr.push(this.keyMap[i][j][1]);
+          }
+        }
+      }
+    }
+    return valuesArr;
+  }
 }
 
 let ht = new HashTable(7);
+ht.set('white', '#ffffff');
 ht.set('white', '#ffffff');
 ht.set('blue', '#0000ff');
 ht.set('green', '#00ff00');
 ht.set('red', '#ff0000');
 ht.set('olive', '#808000');
+ht.set('yellow', '#808000');
+ht.set('maroon', '#808000');
 console.log(ht);
 
 console.log(ht.get('olive')); // #808000
+console.log(ht.keys()); // ['green', 'maroon', 'yellow', 'olive', 'white', 'blue', 'red']
+console.log(ht.values()); // ['#00ff00', '#808000', '#ffffff', '#0000ff', '#ff0000']
