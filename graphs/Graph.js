3f05020888a7fd4389ead7a5aabafe8b1dba2137
Dan <dantheado@gmail.com>
2021-09-29 12:02:16 +0100

    Graph DS and add vertex method

---
+++ graphs/Graph.js
@@ -0,0 +1,16 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+}
+
+let g = new Graph();
+g.addVertex('Tokyo');
+
+console.log(g);
